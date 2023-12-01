8409c0959bcf1676fba775a4cdca018b5da67cc1
Anas Ajaanan <aajaanan@lab3r5s14.42abudhabi.ae>
2023-12-01 15:52:24 +0400

    Update ex02 of CPP07

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -1,53 +1,6 @@
 #include "Array.hpp"
 #include <stdexcept>
 
-template <class T>
-Array<T>::Array() : _size(0), array(NULL) { }
-
-template <class T>
-Array<T>::Array(unsigned int size) : _size(size), array(new T[size]()) { }
-
-template <class T>
-Array<T>::Array(const Array &other) : _size(other._size), array(new T[other._size]())
-{
-	for (unsigned int i = 0; i < _size; i++)
-		array[i] = other.array[i];
-}
-
-template <class T>
-Array<T>	&Array<T>::operator=(const Array &other)
-{
-	if (this != &other)
-	{
-		delete[] array;
-		_size = other._size;
-		array = new T[other._size]();
-		for (unsigned int i = 0; i < _size; i++)
-			array[i] = other.array[i];
-	}
-	return (*this);
-}
-
-template <class T>
-Array<T>::~Array()
-{
-	delete[] array;
-}
-
-template <class T>
-unsigned int	Array<T>::size() const
-{
-	return (_size);
-}
-
-template <class T>
-T	&Array<T>::operator[](unsigned int index)
-{
-	if (index >= _size)
-		throw (std::out_of_range("Index out of range"));
-	return (array[index]);
-}
-
 int main()
 {
 	Array<int> a(5);
